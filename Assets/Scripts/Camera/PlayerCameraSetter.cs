9c5a5ba4f6f77295700b41b10e7212df7ff07740
Zoraah <vovikche@gmail.com>
2023-10-24 22:12:08 +0300

    feat (player): added logic of setting camera target for player

---
+++ Assets/Scripts/Camera/PlayerCameraSetter.cs
@@ -0,0 +1,28 @@
+using Cinemachine;
+using ClansWars.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClansWars.Camera
+{
+    public class PlayerCameraSetter : MonoBehaviour
+    {
+        [SerializeField]
+        private CinemachineVirtualCamera _cinemachineVirtualCamera;
+
+        [SerializeField]
+        private PlayerState _playerState;
+
+        private void Start()
+        {
+            SetPlayerTargetAndFollow(_playerState);
+        }
+
+        public void SetPlayerTargetAndFollow(PlayerState playerState)
+        {
+            _cinemachineVirtualCamera.Follow = playerState.transform;
+            _cinemachineVirtualCamera.LookAt = playerState.transform;
+        }
+    }
+}

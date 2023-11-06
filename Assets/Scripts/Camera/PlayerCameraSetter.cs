b8def505684d940555210c5fc3473a869a4e5b58
Volodymyr Chernyavskyi <vovikche@gmail.com>
2023-11-06 11:24:30 +0200

    feat (players): added logic of local setting camera for players

--- Assets/Scripts/Camera/PlayerCameraSetter.cs
+++ Assets/Scripts/Camera/PlayerCameraSetter.cs
@@ -2,6 +2,7 @@ using Cinemachine;
 using ClansWars.Player;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace ClansWars.Camera
@@ -14,10 +15,28 @@ namespace ClansWars.Camera
         [SerializeField]
         private PlayerState _playerState;
 
+        private void Start()
+        {
+            CheckOwnPlayer();
+        }
+
+        private void CheckOwnPlayer()
+        {
+            if (_playerState.OwnerClientId == NetworkManager.Singleton.LocalClientId)
+            {
+                SetPlayerTargetAndFollow(_playerState);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
         public void SetPlayerTargetAndFollow(PlayerState playerState)
         {
             _cinemachineVirtualCamera.Follow = playerState.transform;
             _cinemachineVirtualCamera.LookAt = playerState.transform;
+            transform.parent = null;
         }
     }
 }

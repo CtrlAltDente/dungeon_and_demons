a070d4a3755e660b638834a26df1a01a6d6ed169
Volodymyr Chernyavskyi <vovikche@gmail.com>
2023-11-10 16:07:49 +0200

    feat (player): improvements
    - now logic of movement and attack depends on animator values, all input fixing doing in PlayerModel script

--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -4,37 +4,37 @@ using ClansWars.Weapons;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace ClansWars.Player
 {
     public class PlayerModel : MonoBehaviour, IPlayerLogicPart
     {
         [SerializeField]
+        private Transform _modelParentObject;
+        [SerializeField]
         private Animator _animator;
         [SerializeField]
         private ModelAnimations _modelAnimations;
 
+        [SerializeField]
+        private PlayerMovement _playerMovement;
+
         public void SetPlayerInputData(PlayerInputData playerInputData)
         {
-            SetMovementValue(playerInputData);
-            SetAttackValue(playerInputData);
+            _animator.SetBool("IsMoving", playerInputData.MovementVector.magnitude > 0.1f);
+            _animator.SetBool("IsPrimaryAttack", playerInputData.IsPrimaryAttack);
+            _animator.SetBool("IsRoll", playerInputData.IsRoll);
+
+            SetFixedMovement(playerInputData);
         }
 
-        private void SetMovementValue(PlayerInputData playerInputData)
+        private void SetFixedMovement(PlayerInputData playerInputData)
         {
-            if (!playerInputData.IsPrimaryAttack)
-            {
-                _animator.SetBool("IsMoving", playerInputData.MovementVector.magnitude > 0.1f);
-            }
-            else
-            {
-                _animator.SetBool("IsMoving", false);
-            }
-        }
+            playerInputData.MovementVector = _animator.GetBool("InMovement") ? playerInputData.MovementVector : Vector2.zero;
+            playerInputData.IsRoll = _animator.GetBool("InRoll");
 
-        private void SetAttackValue(PlayerInputData playerInputData)
-        {
-            _animator.SetBool("IsAttack", playerInputData.IsPrimaryAttack);
+            _playerMovement.SetPlayerInputData(playerInputData);
         }
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -20,20 +20,21 @@ namespace ClansWars.Player
         [SerializeField]
         private float _rotationSpeed;
 
-        [SerializeField]
         private bool _isRolling;
 
         private void Update()
         {
-            if (!_currentPlayerInput.IsPrimaryAttack)
-            {
-                Move(_currentPlayerInput);
-            }
+            Move(_currentPlayerInput);
         }
 
         public void SetPlayerInputData(PlayerInputData playerInputData)
         {
-            _currentPlayerInput = playerInputData;
+            _isRolling = playerInputData.IsRoll;                        //roll has more priority because roll blocking input (usually in games)
+
+            if (!_isRolling)
+            {
+                _currentPlayerInput = playerInputData;
+            }
         }
 
         private void Move(PlayerInputData playerInputData)
@@ -42,8 +43,10 @@ namespace ClansWars.Player
             {
                 Vector3 movementVector3 = new Vector3(playerInputData.MovementVector.x, 0f, playerInputData.MovementVector.y);
                 _playerTransform.rotation = Quaternion.RotateTowards(_playerTransform.rotation, Quaternion.LookRotation(movementVector3), _rotationSpeed * Time.deltaTime);
-                _playerTransform.position += _playerTransform.forward * _movementSpeed * Time.deltaTime;
+                _playerTransform.position += _playerTransform.forward * _movementSpeed * CalculatedRollSpeed * Time.deltaTime;
             }
         }
+
+        private float CalculatedRollSpeed => _isRolling ? 2 : 1;
     }
 }

6828cabdc28e35e8d352e63027edc2b2837fb652
Volodymyr Chernyavskyi <vovikche@gmail.com>
2023-11-13 15:57:25 +0200

    feat (player): created base attack logic

--- Assets/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -6,9 +6,29 @@ namespace ClansWars.Weapons
 {
     public class MeleeWeapon : Weapon
     {
+        public float AttackRange;
+
+        [SerializeField]
+        private Collider _collider;
+
         public override void Attack()
         {
-            
+            _collider.enabled = true;
+            StartCoroutine(DisableCollider());
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.GetComponent<IDamagable>() != null)
+            {
+                other.GetComponent<IDamagable>().TakeDamage(_damage);
+            }
+        }
+
+        private IEnumerator DisableCollider()
+        {
+            yield return new WaitForSeconds(0.1f);
+            _collider.enabled = false;
         }
     }
 }

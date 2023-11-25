56e753a7212b8438414f12e9f5b2470516b70732
CtrlAltDente <vovikche@gmail.com>
2023-11-25 19:15:59 +0200

    chore (scripts): removed unnesessary method

--- Assets/Scripts/Network/Base/LobbyStarter.cs
+++ Assets/Scripts/Network/Base/LobbyStarter.cs
@@ -20,12 +20,6 @@ namespace DungeonAndDemons.Network
             _scenesLoader.FadeOut(() => OpenLobbyScene());
         }
 
-        public void Shutdown()
-        {
-            NetworkManager.Singleton.Shutdown();
-            _scenesLoader.LoadLocalScene("Scene_MainMenu");
-        }
-
         private void OpenLobbyScene()
         {
             if(NetworkManager.Singleton.IsClient)

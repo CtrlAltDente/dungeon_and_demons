2c887f2413a19fced5f26ac46297c9c527900dd9
CtrlAltDente <vovikche@gmail.com>
2023-11-30 10:46:12 +0200

    feat (scripts): added serialization to character stats

--- Assets/Scripts/Player/Character/CharacterItems.cs
+++ Assets/Scripts/Player/Character/CharacterItems.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace DungeonAndDemons.Characters
 {
     [Serializable]
-    public struct CharacterItems
+    public struct CharacterItems : INetworkSerializable
     {
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
 
+        }
     }
 }
--- Assets/Scripts/Player/Character/CharacterVisualConfiguration.cs
+++ Assets/Scripts/Player/Character/CharacterVisualConfiguration.cs
@@ -1,17 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace DungeonAndDemons.Characters
 {
     [Serializable]
-    public struct CharacterVisualConfiguration
+    public struct CharacterVisualConfiguration : INetworkSerializable
     {
         public int HairStyleIndex;
         public int EyebrowIndex;
         public int BeardIndex;
         public Color HairColor;
         public int SuitIndex;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref HairStyleIndex);
+            serializer.SerializeValue(ref EyebrowIndex);
+            serializer.SerializeValue(ref BeardIndex);
+            serializer.SerializeValue(ref HairColor);
+            serializer.SerializeValue(ref SuitIndex);
+        }
     }
 }

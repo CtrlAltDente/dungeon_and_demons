19e94e107e2ba7da3a8740db69b48b530840a06c
CtrlAltDente <vovikche@gmail.com>
2023-12-02 12:59:12 +0200

    feat (scripts): created childer script of generic ItemObject for every item type

---
+++ Assets/Scripts/Items/World objects/WeaponObject.cs
@@ -0,0 +1,12 @@
+using DungeonAndDemons.ScriptableObjects.Items;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonAndDemons.Items
+{
+    public class WeaponObject : ItemObject<WeaponItem>
+    {
+
+    }
+}

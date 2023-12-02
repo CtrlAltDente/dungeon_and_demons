11638aa19e06e3b2f4fbd49395a533413648d11e
CtrlAltDente <vovikche@gmail.com>
2023-12-02 14:51:54 +0200

    feat (scripts): added to ItemObject logic of setting mesh and materials of item

--- Assets/Scripts/Items/World objects/ItemObject.cs
+++ Assets/Scripts/Items/World objects/ItemObject.cs
@@ -10,5 +10,29 @@ namespace DungeonAndDemons.Items
     {
         public ItemInfo Info;
         public T Item;
+
+        [SerializeField]
+        private MeshFilter _meshFilter;
+        [SerializeField]
+        private MeshRenderer _meshRenderer;
+
+        [SerializeField]
+        private MeshCollider _meshCollider;
+
+        private void Start()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _meshFilter.mesh = Item.Model.Mesh;
+            _meshRenderer.materials = Item.Model.Model.GetComponent<MeshRenderer>().sharedMaterials;
+
+            _meshCollider.sharedMesh = Item.Model.Mesh;
+
+            transform.position += Item.Model.PositionOffset;
+            transform.rotation *= Quaternion.Euler(Item.Model.RotationOffset);
+        }
     }
 }

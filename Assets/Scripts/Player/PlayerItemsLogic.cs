7496a947aa318288951bfa46f01e91a91bbc84ee
CtrlAltDente <vovikche@gmail.com>
2023-12-04 17:43:51 +0200

    feat (scripts): configured finding items by interface

--- Assets/Scripts/Player/PlayerItemsLogic.cs
+++ Assets/Scripts/Player/PlayerItemsLogic.cs
@@ -2,6 +2,7 @@ using DungeonAndDemons.Interfaces;
 using DungeonAndDemons.Items;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,13 +10,16 @@ namespace DungeonAndDemons.Player
 {
     public class PlayerItemsLogic : MonoBehaviour, IPlayerLogicPart
     {
-        public UnityEvent<ItemObject<IItemBase>> OnItemPicked;
+        public UnityEvent<IItemObject<IItemBase>> OnItemPicked;
 
-        private List<ItemObject<IItemBase>> AvailableItems;
+        private List<IItemObject<IItemBase>> AvailableItems = new List<IItemObject<IItemBase>>();
+
+        [SerializeField]
+        private List<GameObject> gameObjects = new List<GameObject>();
 
         public void SetPlayerInputData(PlayerInputData playerInputData)
         {
-            if(playerInputData.ScrollValue > 0)
+            if (playerInputData.ScrollValue > 0)
             {
                 PickupItem();
             }
@@ -31,28 +35,54 @@ namespace DungeonAndDemons.Player
 
         private void OnTriggerEnter(Collider other)
         {
-            ItemObject<IItemBase> possibleItem = other.GetComponent<ItemObject<IItemBase>>();
-
-            if (possibleItem != null)
-            {
-                if (!AvailableItems.Contains(possibleItem))
-                {
-                    AvailableItems.Add(possibleItem);
-                }
-            }
+            AddPossibleItems(other);
+            Debug.Log($"Added. Items: {AvailableItems.Count}");
         }
 
         private void OnTriggerExit(Collider other)
         {
-            ItemObject<IItemBase> possibleItem = other.GetComponent<ItemObject<IItemBase>>();
+            RemovePossibleItems(other);
+            Debug.Log($"Removed. Items: {AvailableItems.Count}");
+        }
 
-            if (possibleItem != null)
-            {
-                if (AvailableItems.Contains(possibleItem))
-                {
-                    AvailableItems.Remove(possibleItem);
-                }
-            }
+        private void AddPossibleItems(Collider other)
+        {
+            if (AddItem(other.GetComponent<AccessoryObject>()))
+                gameObjects.Add(other.gameObject);
+            if (AddItem(other.GetComponent<SuitObject>()))
+                gameObjects.Add(other.gameObject);
+            if (AddItem(other.GetComponent<WeaponObject>()))
+                gameObjects.Add(other.gameObject);
+        }
+
+        private void RemovePossibleItems(Collider other)
+        {
+            if (RemoveItem(other.GetComponent<AccessoryObject>()))
+                gameObjects.Remove(other.gameObject);
+            if (RemoveItem(other.GetComponent<SuitObject>()))
+                gameObjects.Remove(other.gameObject);
+            if (RemoveItem(other.GetComponent<WeaponObject>()))
+                gameObjects.Remove(other.gameObject);
+        }
+
+        private bool AddItem(IItemObject<IItemBase> itemObject)
+        {
+            if (itemObject == null || AvailableItems.Contains(itemObject))
+                return false;
+
+            AvailableItems.Add(itemObject);
+
+            return true;
+        }
+
+        private bool RemoveItem(IItemObject<IItemBase> itemObject)
+        {
+            if (itemObject == null || !AvailableItems.Contains(itemObject))
+                return false;
+
+            AvailableItems.Remove(itemObject);
+
+            return true;
         }
     }
 }

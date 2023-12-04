77a52a2d3c077e48f1b80836837aa39e943bc1ed
CtrlAltDente <vovikche@gmail.com>
2023-12-04 13:07:31 +0200

    feat (scripts): created base logic of setting items

--- Assets/Scripts/Character/PlayerCharacter.cs
+++ Assets/Scripts/Character/PlayerCharacter.cs
@@ -1,19 +1,46 @@
+using DungeonAndDemons.Interfaces;
 using DungeonAndDemons.Items;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace DungeonAndDemons.Player
 {
     public class PlayerCharacter : MonoBehaviour
     {
+        public UnityEvent<ItemSlot> OnItemSlotUpdated;
+
+        [SerializeField]
         public PlayerInfo PlayerInfo;
 
-        public Animator Animator;
-
-        public void UpdateItemInfo(ItemType itemType, ItemStats itemStats)
+        public void SetItem(ItemObject<IItemPreferences> itemObject)
         {
+            try
+            {
+                ItemSlot itemSlot = GetItemSlotByType(itemObject.Type);
+                itemSlot.ItemIndex = itemObject.Info.ItemIndex;
 
+                OnItemSlotUpdated?.Invoke(itemSlot);
+            }
+            catch(Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+
+        private ItemSlot GetItemSlotByType(ItemType itemType)
+        {
+            foreach(ItemSlot itemSlot in PlayerInfo.ItemSlots)
+            {
+                if(itemSlot.ItemType == itemType)
+                {
+                    return itemSlot;
+                }
+            }
+
+            return null;
         }
     }
 }

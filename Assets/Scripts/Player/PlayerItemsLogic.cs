4e32c469a13842b92953e945aabc1ca2b8d84104
CtrlAltDente <vovikche@gmail.com>
2023-12-04 10:22:22 +0200

    feat (scripts): created logic of picking items in PlayerItemsLogic

--- Assets/Scripts/Player/PlayerItemsLogic.cs
+++ Assets/Scripts/Player/PlayerItemsLogic.cs
@@ -1,3 +1,4 @@
+using DungeonAndDemons.Interfaces;
 using DungeonAndDemons.Items;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,8 +7,53 @@ using UnityEngine.Events;
 
 namespace DungeonAndDemons.Player
 {
-    public class PlayerItemsLogic : MonoBehaviour
+    public class PlayerItemsLogic : MonoBehaviour, IPlayerLogicPart
     {
+        public UnityEvent<ItemObject<IItemPreferences>> OnItemPicked;
 
+        [SerializeField]
+        private List<ItemObject<IItemPreferences>> AvailableItems;
+
+        public void SetPlayerInputData(PlayerInputData playerInputData)
+        {
+            if(playerInputData.ScrollValue > 0)
+            {
+                PickupItem();
+            }
+        }
+
+        private void PickupItem()
+        {
+            if (AvailableItems.Count > 0)
+            {
+                OnItemPicked?.Invoke(AvailableItems[0]);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            ItemObject<IItemPreferences> possibleItem = other.GetComponent<ItemObject<IItemPreferences>>();
+
+            if (possibleItem != null)
+            {
+                if (!AvailableItems.Contains(possibleItem))
+                {
+                    AvailableItems.Add(possibleItem);
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            ItemObject<IItemPreferences> possibleItem = other.GetComponent<ItemObject<IItemPreferences>>();
+
+            if (possibleItem != null)
+            {
+                if (AvailableItems.Contains(possibleItem))
+                {
+                    AvailableItems.Remove(possibleItem);
+                }
+            }
+        }
     }
 }

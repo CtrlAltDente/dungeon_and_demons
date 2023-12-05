4c9d2e3068779faa0a63c62668135666ede3a794
CtrlAltDente <vovikche@gmail.com>
2023-12-05 22:41:30 +0200

    feat (scripts): added serialiczable attribute

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,10 +1,12 @@
 using DungeonAndDemons.ScriptableObjects;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace DungeonAndDemons.Items
 {
+    [Serializable]
     public class Item : MonoBehaviour
     {
         public ItemType Type;

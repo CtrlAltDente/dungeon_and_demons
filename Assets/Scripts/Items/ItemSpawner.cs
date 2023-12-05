126c1f172349225b7cfd549de5ccd81c567c3555
CtrlAltDente <vovikche@gmail.com>
2023-12-05 22:41:57 +0200

    feat (scripts): configured ItemSpawner logic

--- Assets/Scripts/Items/ItemSpawner.cs
+++ Assets/Scripts/Items/ItemSpawner.cs
@@ -1,9 +1,51 @@
+using DungeonAndDemons.ScriptableObjects;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace DungeonAndDemons.Items
 {
     public class ItemSpawner : MonoBehaviour
     {
-        
+        [SerializeField]
+        private ItemObject _itemObject;
+
+        [SerializeField]
+        private Item _itemPrefab;
+
+        public ItemPreset ItemPreset;
+
+        private void Start()
+        {
+            SpawnPrefab(ItemPreset);
+        }
+
+        public void SpawnPrefab(ItemPreset itemPreset)
+        {
+            ItemObject spawnedItemObject = Instantiate(_itemObject, transform.position, Quaternion.identity, null);
+
+            Item spawnedItem = spawnedItemObject.gameObject.AddComponent<Item>();
+
+            spawnedItem.Type = itemPreset.Type;
+            spawnedItem.Model = itemPreset.Model;
+            spawnedItem.Modifiers = itemPreset.Modifiers;
+
+            spawnedItemObject.Item = spawnedItem;
+        }
+    }
+
+    [Serializable]
+    public struct ItemPreset
+    {
+        public ItemType Type;
+        public ItemModel Model;
+        public List<Modifier> Modifiers;
+
+        public ItemPreset(ItemType itemType, ItemModel itemModel, List<Modifier> modifiers)
+        {
+            Type = itemType;
+            Model = itemModel;
+            Modifiers = modifiers;
+        }
     }
 }

012655623434f8b326b4c9880b39a6ecc421a94d
CtrlAltDente <vovikche@gmail.com>
2023-12-05 20:55:15 +0200

    feat (scripts): added CreateAssetMenu attribute

--- Assets/Scripts/ScriptableObjects/Containers/Items/ItemsContainer.cs
+++ Assets/Scripts/ScriptableObjects/Containers/Items/ItemsContainer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 namespace DungeonAndDemons.ScriptableObjects.Containers
 {
+    [CreateAssetMenu(fileName = "Container_", menuName = "Scriptable Objects/Containers/Items", order = 1)]
     public class ItemsContainer : Container<Item>
     {
 

3b9d46459a282fbc540303f668be752e11a84b41
CtrlAltDente <vovikche@gmail.com>
2023-12-05 21:33:48 +0200

    feat (scripts): created armor modifier

---
+++ Assets/Scripts/ScriptableObjects/Modifiers/ArmorModifier.cs
@@ -0,0 +1,15 @@
+using DungeonAndDemons.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonAndDemons.ScriptableObjects.Modifiers
+{
+    public class ArmorModifier : Modifier
+    {
+        public override void ApplyModifier(PlayerInfo playerInfo)
+        {
+
+        }
+    }
+}
